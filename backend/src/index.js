f6f86c8a776948442a4ca2bc5e477f6ee4e50567
sorheil <sorheiltchokothe@gmail.com>
2025-12-07 10:46:02 +0100

    set up all admin routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,9 @@ import { serve } from "inngest/express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 
+//routes
+import adminRoute from "./routes/admin.route.js";
+
 const app = express();
 
 const __dirname = path.resolve();
@@ -24,6 +27,9 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 app.get("/api/health", (req, res) => {
 	res.status(200).json({ message: "Success" });
 });
+
+app.use("/api/admin", adminRoute);
+
 
 //make the app ready for the deployment
 if (ENV.NODE_ENV === "production") {
